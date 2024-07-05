feb4787658eec3435c91c0a8144a90cd77ada10d
De1t4 <mauriciochambicaceres@gmail.com>
2024-07-05 02:23:17 -0300

    fix error eslint

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <section className='animate-fadeIn' id='about'>
       <h1 className=' text-3xl font-bold mb-2'>About Me</h1>
-      <p >Hi, I'm John Doe, a passionate web developer with a love for creating beautiful and functional websites. I have a wide range of interests and enjoy exploring new technologies and techniques.</p>
+      <p >Hi, Im John Doe, a passionate web developer with a love for creating beautiful and functional websites. I have a wide range of interests and enjoy exploring new technologies and techniques.</p>
     </section>
   )
 }
